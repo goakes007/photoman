53ddb1f6077f0343c552916e631f11092d05ea93
goakes007 <goakes007@gmail.com>
2018-05-20 11:24:51 -0400

    Added test photos and config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration details can be inserted here for photoman
+
+BLACKANDWHITE = 1
+GREY = 2
+COLOR = 3
+
+photodir = "photo"
+default_grade = BLACKANDWHITE
+default_testing = False
+
+
